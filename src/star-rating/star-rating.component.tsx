4975e9e9a68492e6240e685b635ed85d0a1653e9
femjot <reactventi@email.com>
2024-07-17 11:49:42 +0200

    implementing hovered rating

--- src/star-rating/star-rating.component.tsx
+++ src/star-rating/star-rating.component.tsx
@@ -3,24 +3,38 @@ import "./star-rating.styles.css";
 
 const StarRating = () => {
   const [currentRating, setCurrentRating] = useState(0);
+  const [hoveredRating, setHoveredRating] = useState(0);
   const maxRating = 5;
 
-  const setCurrentRatingClickHandler = (ratingValue: number) => {
+  const handleStarClick = (ratingValue: number) => {
     ratingValue === currentRating
       ? setCurrentRating(0)
       : setCurrentRating(ratingValue);
   };
 
+  const handleStarMouseOver = (ratingValue: number) => {
+    setHoveredRating(ratingValue);
+  };
+
+  const handleStarContainerMouseOut = () => {
+    setHoveredRating(0);
+  };
+
   return (
-    <div className="star-rating-container">
+    <div
+      className="star-rating-container"
+      onMouseOut={() => handleStarContainerMouseOut()}
+    >
       currentRating: {currentRating}
+      hoverRating: {hoveredRating}
       {[...Array(maxRating)].map((_, idx) => {
         const ratingValue: number = idx + 1;
         return (
           <p
             key={idx}
-            onClick={() => setCurrentRatingClickHandler(ratingValue)}
-            className={`star ${ratingValue <= currentRating ? "active" : ""}`}
+            onClick={() => handleStarClick(ratingValue)}
+            onMouseOver={() => handleStarMouseOver(ratingValue)}
+            className={`star ${ratingValue <= (hoveredRating || currentRating) ? "active" : ""}`}
           >
             {ratingValue}
           </p>
